25a31fc7eb30c74af0629e6eeb3259487452a521
DevendraBAJAJ <dmbajaj.bajaj@gmail.com>
2022-02-16 17:05:47 +0530

    finished header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com', 'user-images.githubusercontent.com'],
+  }
 }
