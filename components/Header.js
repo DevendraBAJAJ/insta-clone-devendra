8c5c442409a764637bfb640c5bf926859549f548
DevendraBAJAJ <dmbajaj.bajaj@gmail.com>
2022-02-16 16:01:52 +0530

    added Header placeholder

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,13 @@
+import Image from "next/image"
+
 function Header() {
   return (
     <div>
       <h1>This is the header</h1>
       {/* Left */}
+      <div>
+          <Image src='https://' />
+      </div>
       {/* Middle */}
       {/* Right */}
     </div>
