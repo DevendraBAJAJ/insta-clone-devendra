f00f109a0bff602ee026b593800778aa0e63f8e2
DevendraBAJAJ <dmbajaj.bajaj@gmail.com>
2022-02-16 16:01:08 +0530

    first commit

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div>
+      <h1>This is the header</h1>
+      {/* Left */}
+      {/* Middle */}
+      {/* Right */}
+    </div>
+  )
+}
+
+export default Header
