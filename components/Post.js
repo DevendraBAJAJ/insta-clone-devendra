cbe4a267dc5e7edd62182d797e1a9c0981f05107
DevendraBAJAJ <dmbajaj.bajaj@gmail.com>
2022-02-20 18:42:25 +0530

    scale stories

--- components/Post.js
+++ components/Post.js
@@ -11,7 +11,7 @@ import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
 
 function Post({id, username, userImg, img, caption}) {
   return (
-    <div className='ng-white my-7 border rounded-sm'>
+    <div className='ng-white my-7 border rounded-sm'> 
       <div className='flex items-center p-5'>
           <img 
           src={userImg} 
