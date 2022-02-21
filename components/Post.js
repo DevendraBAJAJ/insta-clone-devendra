1b17fe015cd96918d8f3e699d007942dd884e490
DevendraBAJAJ <dmbajaj.bajaj@gmail.com>
2022-02-21 15:16:13 +0530

    Posts

--- components/Post.js
+++ components/Post.js
@@ -13,7 +13,7 @@ function Post({id, username, userImg, img, caption}) {
   return (
     <div className='ng-white my-7 border rounded-sm'> 
       <div className='flex items-center p-5'>
-          <img 
+          <img
           src={userImg} 
           className='rounded-full h-12 w-12 object-contain
           border p-1 mr-3'
