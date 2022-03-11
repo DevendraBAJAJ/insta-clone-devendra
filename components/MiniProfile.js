1c72e7f7e9311e5989d65474d2cf4dcf089e6d12
DevendraBAJAJ <dmbajaj.bajaj@gmail.com>
2022-03-11 11:17:54 +0530

    next auth google provider and firebase implementation, sessions and signin signout functionality, session variables mofification

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,14 +1,20 @@
+import { signOut, useSession } from "next-auth/react"
+
 function MiniProfile() {
+  const {data: session} = useSession();
+
+  console.log(session);
+    
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
         <img 
         className="rounded-full border p-[2px] w-16 h-16" 
-        src="https://user-images.githubusercontent.com/16799932/154198093-6951995c-ca8f-41fe-a0a7-1399c4a911ae.jpeg" />
+        src={session?.user?.image} />
         <div className="flex-1 mx-4">
-            <h2 className="font-bold">devendra77</h2>
+            <h2 className="font-bold">{session?.user?.username}</h2>
             <h3 className="text-sm text-gray-400">Welcome to Instagram!</h3>
         </div>
-        <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+        <button onClick={signOut} className="text-blue-400 text-sm font-semibold">Sign Out</button>
     </div>
   )
 }
